c28fadb03e206b050155cfbec8d7513c36988164
jaromajdz <jaromajdz@gmail.com>
2023-12-31 06:08:14 +0100

    firebase.ts

---
+++ src/app/firebase/firebase.ts
@@ -0,0 +1,12 @@
+export const environment = { 
+    production: false,
+    firebase: {
+    apiKey: "AIzaSyBLgMaypwbtdnxYclS_B_Pu5tPCvH_r57M",
+    authDomain: "login-4a040.firebaseapp.com",
+    databaseURL: "https://login-4a040-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "login-4a040",
+    storageBucket: "login-4a040.appspot.com",
+    messagingSenderId: "269412976790",
+    appId: "1:269412976790:web:0ab03dba148062f58b5ab7"
+  }
+};
